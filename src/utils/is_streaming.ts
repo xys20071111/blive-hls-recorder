64f2e1d90b46a5c1813e9552a349612f4b6554e0
xys20071111 <xys20071111@hotmail.com>
2024-07-16 17:26:36 +0800

    Revert "接口好像改了,修一下程序"
    This reverts commit 55604c86fb372959e2d2e87c1f15708782e4a5ea.

--- src/utils/is_streaming.ts
+++ src/utils/is_streaming.ts
@@ -2,10 +2,7 @@ import { request } from './requester.ts'
 import { RoomInfo } from '../IMsg.ts'
 
 export async function isStreaming(id: number) {
-    const data = await request('/xlive/web-room/v2/index/getRoomPlayInfo', 'GET', {
-        room_id: id,
-        no_playurl: 1
-    })
+    const data = await request('/room/v1/Room/room_init', 'GET', { id })
     if (!data) {
         return false
     }
